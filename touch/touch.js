29e859c84161a5050cc33e433ade8a15372a309c
mucriva <57014703+mucriva@users.noreply.github.com>
2019-11-29 13:10:06 +0200

    Add files via upload

---
+++ touch/touch.js
@@ -0,0 +1,17 @@
+document.getElementById("id_logic").innerHTML = "Logic version = 2019.11.29.0";
+
+window.addEventListener("touchstart", touch_start_uab);
+
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+
+var client_rect = canvas.getBoundingClientRect();
+
+function touch_start_uab(e)
+{
+	var t = e.changedTouches;
+	for (var i = 0; i < t.lenght; i++){
+		context.beginPath();
+		context.arc(t[i].pageX - client_rect.left, t[i].pageY - client_rect.top, 10, 0, 2 * Math.PI);
+	}
+}
